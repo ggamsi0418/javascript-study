cc2b54239bdf90b47549ed0bcda8187b1221e15f
ggamsi0418 <58096698+ggamsi0418@users.noreply.github.com>
2020-06-03 20:06:17 +0900

    [Web 2 Node.js & MySQL] Section 5~6. 보안&마무리

--- web2-node-db/README.md
+++ web2-node-db/README.md
@@ -8,3 +8,15 @@ https://www.evernote.com/l/AlPvaxR4a4NKzp8EJayH42lbeuvguoKYEoE/
 
 ### [생활코딩 - Web 2 Node.js & MySQL] Section 2. MySQL Join을 이용해서 구현
 https://www.evernote.com/l/AlPH-91lxtNEqo5CZmjhICQplJ-6zSmmiiM/
+
+### [생활코딩 - Web 2 Node.js & MySQL] Section 3. Node.js 정리정돈
+None
+
+### [생활코딩 - Web 2 Node.js & MySQL] Section 4. 저자 관련 기능 구현
+None
+
+### [생활코딩 - Web 2 Node.js & MySQL] Section 5. 보안
+None
+
+### [생활코딩 - Web 2 Node.js & MySQL] Section 6. 마무리
+None
--- web2-node-db/lib/topic.js
+++ web2-node-db/lib/topic.js
@@ -2,6 +2,7 @@ var db = require('./db.js')
 var template = require('./template.js');
 var url = require('url');
 var qs = require('querystring');
+var sanitizeHtml = require('sanitize-html');
 
 
 
@@ -10,7 +11,7 @@ exports.home = function (request, response) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
         var list = template.list(topics);
-        var html = template.HTML(title, list,
+        var html = template.HTML(sanitizeHtml(title), list,
             `<h2>${title}</h2>${description}`,
             `<a href="/create">create</a>`
         );
@@ -34,9 +35,9 @@ exports.page = function (request, response) {
             var description = topic[0].description;
             var list = template.list(topics);
             var html = template.HTML(title, list,
-                `<h2>${title}</h2>
-                 ${description}
-                 <p>by ${topic[0].name}</p>
+                `<h2>${sanitizeHtml(title)}</h2>
+                 ${sanitizeHtml(description)}
+                 <p>by ${sanitizeHtml(topic[0].name)}</p>
                 `,
                 ` <a href="/create">create</a>
                   <a href="/update?id=${queryData.id}">update</a>
@@ -56,7 +57,7 @@ exports.create = function (request, response) {
         db.query('SELECT * FROM author', function (err2, authors) {
             var title = 'Create';
             var list = template.list(topics);
-            var html = template.HTML(title, list,
+            var html = template.HTML(sanitizeHtml(title), list,
                 `<form action="/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
@@ -115,13 +116,13 @@ exports.update = function (request, response) {
             }
             db.query('SELECT * FROM author', function (err2, authors) {
                 var list = template.list(topics);
-                var html = template.HTML(topic[0].title, list,
+                var html = template.HTML(sanitizeHtml(topic[0].title), list,
                     `
               <form action="/update_process" method="post">
                 <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                <p><input type="text" name="title" placeholder="title" value="${sanitizeHtml(topic[0].title)}"></p>
                 <p>
-                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                  <textarea name="description" placeholder="description">${sanitizeHtml(topic[0].description)}</textarea>
                 </p>
                 <p>
                   ${template.authorSelect(authors, topic[0].author_id)}
