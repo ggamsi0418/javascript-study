c451c4759c4bb4395660a2200856dac4e28fe123
ggamsi0418 <58096698+ggamsi0418@users.noreply.github.com>
2020-06-03 19:32:41 +0900

    [Web 2 Node.js & MySQL] Section 4. 저자 관련 기능 구현

--- web2-node-db/lib/template.js
+++ web2-node-db/lib/template.js
@@ -9,6 +9,7 @@ module.exports = {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
       ${list}
       ${control}
       ${body}
@@ -31,18 +32,38 @@ module.exports = {
     var i = 0;
     while (i < authors.length) {
       var selected = '';
-      console.log("check01");
       if (authors[i].id === author_id) {
-        console.log("check02");
         selected = ' selected';
       }
       tag += `<option value="${authors[i].id}"${selected}>${authors[i].name}</option>`;
       i++;
-    };
+    }
     return `
       <select name="author">
         ${tag}
       </select>
-      `
+    `
+  },
+  authorTable: function (authors) {
+    var tag = '<table>';
+    var i = 0;
+    while (i < authors.length) {
+      tag += `
+            <tr>
+                <td>${authors[i].name}</td>
+                <td>${authors[i].profile}</td>
+                <td><a href="/author/update?id=${authors[i].id}">update</a></td>
+                <td>
+                  <form action="/author/delete_process" method="post">
+                    <input type="hidden" name="id" value="${authors[i].id}">
+                    <input type="submit" value="delete">
+                  </form>
+                </td>
+            </tr>
+            `
+      i++;
+    }
+    tag += '</table>';
+    return tag;
   }
 }
