1d91631bdffbfe6e37568026afbe680428c130f4
ggamsi0418 <58096698+ggamsi0418@users.noreply.github.com>
2020-06-19 14:56:22 +0900

    [테스트주도개발(TDD)로 만드는 NodeJS API 서버] Section 9. 데이터베이스(추가)

--- nodejs-api-tdd/bin/sync-db.js
+++ nodejs-api-tdd/bin/sync-db.js
@@ -1,9 +1,10 @@
 const models = require('../models')
 
 module.exports = () => {
-    return models.sequelize.sync({
-        force: true // 기존의 db를 날려버리고 다시 새로 만든다는 의미.
-    })
+    const options = {
+        force: process.env.NODE_ENV === 'test' ? true : false // 기존의 db를 날려버리고 다시 새로 만든다는 의미.
+    }
+    return models.sequelize.sync(options)
     //  models.sequelize.sync는 내부적으로 promise를 리턴하게 되어 있다.
     // 그래서 비동기 처리를 완료할 수 있도록 인터페이스를 제공한다.
 }
